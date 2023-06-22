35d9aaf5c78882d4d28e83ac68bf5f5e41aa00dd
Harshroxnox <harshroxnox@gmail.com>
2023-06-23 00:26:54 +0530

    implemented isEmpty, isFull, enqueue and display functions to the array implentation of queue

--- queue/queue.c
+++ queue/queue.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 struct queue {
     int front;
@@ -18,4 +19,66 @@ struct queue* createQueue(int size){
     
     queue->array = (int *)malloc(size * sizeof(int));
     return queue;    
+}
+
+int isEmpty(struct queue* queue){
+    if(queue->elements == 0){
+        return 1;
+    }else{
+        return 0;
+    }
+}
+
+int isFull(struct queue* queue){
+    if(queue->elements == queue->size){
+        return 1;
+    }else{
+        return 0;
+    }
+}
+
+void enqueue( struct queue* queue, int data){
+    if(isFull(queue)){
+        printf("Cannot Enqueue. Queue full!\n");
+        return;
+    }
+    queue->rear = (queue->rear+1)%(queue->size);
+    *(queue->array + queue->rear) = data;
+    queue->elements++;
+    printf("%d enqueued!\n", data);
+}
+
+
+void display(struct queue* queue){
+    if(isEmpty(queue)){
+        printf("No elements to display!\n");
+        return;
+    }
+    for(int i=queue->front; i<queue->elements; i++){
+        printf(" <-- %d",*(queue->array+i));
+    }
+    printf("\n");
+}
+
+int main(){
+
+    struct queue* queue = createQueue(5);
+
+    enqueue(queue, 13);
+    enqueue(queue, 4);
+    enqueue(queue, 8);
+    enqueue(queue, 12);
+    enqueue(queue, 5);
+    enqueue(queue, 2);
+
+    display(queue);
+
+    printf("front %d\n", queue->front);
+    printf("rear %d\n", queue->rear);
+    printf("elements %d\n", queue->elements);
+
+    
+
+
+    return 0;
 }
