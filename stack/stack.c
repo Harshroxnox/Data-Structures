dff17f412a07cbabf0f95dd11dbc3734131a936c
Harshroxnox <harshroxnox@gmail.com>
2023-06-25 14:45:17 +0530

    improved stack implementation such that multiple stacks can now be created inside stack.c

--- stack/stack.c
+++ stack/stack.c
@@ -6,41 +6,49 @@ struct node {
     struct node* next;
 };
 
-int count = 0;
-struct node* top = NULL;
+struct stack {
+    int count;
+    struct node* top;
+};
+
+struct stack * createStack(){
+    struct stack* st = (struct stack*)malloc(sizeof(struct stack));
+    st->count = 0;
+    st->top = NULL;
+    return st;
+}
 
 int isFull(){
     struct node * ptr = (struct node*)malloc(sizeof(struct node));
     if(ptr == NULL){
         return 1;
     }else{
+        free(ptr);
         return 0;
     }
 }
 
-void push(int data){
-    struct node* newNode = (struct node *)malloc(sizeof(struct node));
-    newNode->data = data;
+void push(struct stack* st, int data){
+
     if(isFull() == 1){
         printf("The stack is already full\nStack Overflow!\n");
+        return;
     }
-    else if(top == NULL){
-        newNode->next = NULL;
-    }else{
-        newNode->next = top;
-    }
-    top = newNode;
-    count++;
+    struct node* newNode = (struct node *)malloc(sizeof(struct node));
+    newNode->data = data;
+    newNode->next = st->top;
+    st->top = newNode;
+    st->count++;
     printf("Pushed %d!\n", data);
 
 }
 
-void display(){
-    if(top == NULL){
+void display(struct stack* st){
+    if(st->top == NULL){
         printf("Stack Empty!\n");
         return;
     }
-    struct node* ptr = top;
+    struct node* ptr = st->top;
     printf("The elements in the stack are:\n");
     while(ptr != NULL){
         printf(" --> %d\n", ptr->data);
@@ -48,50 +56,52 @@ void display(){
     }
 }
 
-void pop(){
-    if(top == NULL){
+void pop(struct stack* st){
+    if(st->top == NULL){
         printf("Stack Empty!\n");
         return;
     }
-    struct node* ptr = top; 
-    top = (top)->next;
+    struct node* ptr = st->top; 
+    st->top = (st->top)->next;
     free(ptr);
-    count--;
+    st->count--;
     printf("Popped!\n");
 }
 
-void peek(){
-    if(top == NULL){
+void peek(struct stack* st){
+    if(st->top == NULL){
         printf("Stack Empty!\n");
         return;
     }
-    printf("First element is: %d\n", top->data);
+    printf("First element is: %d\n", st->top->data);
 }
 
-void isEmpty(){
-    if(top == NULL){
+void isEmpty(struct stack* st){
+    if(st->top == NULL){
         printf("Stack is Empty!\n");
     }else{
         printf("Stack is not Empty!\n");
     }
 }
 
-void numOfElements(){
-    printf("The no. of elements in stack: %d\n", count);
+void numOfElements(struct stack* st){
+    printf("The no. of elements in stack: %d\n", st->count);
 }
 
 int main(){
     
-    pop();
-    push(5);
-    push(14);
-    push(11);
-    push(23);
-    display();
-    numOfElements();
-    pop();
-    pop();
-    display();
+    struct stack* st = createStack();
+
+    pop(st);
+    push(st,5);
+    push(st,14);
+    push(st,11);
+    push(st, 23);
+    display(st);
+    numOfElements(st);
+    pop(st);
+    pop(st);
+    display(st);
 
         
    // The elements in the stack are:
@@ -99,9 +109,9 @@ int main(){
    // --> 5
         
 
-    peek();
-    isEmpty();
-    numOfElements();
+    peek(st);
+    isEmpty(st);
+    numOfElements(st);
     
  
 
