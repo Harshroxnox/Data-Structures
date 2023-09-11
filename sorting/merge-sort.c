fd17c9a48f1c545e8ab76fcf16bacba86499ea7f
Harshroxnox <harshroxnox@gmail.com>
2023-09-11 12:06:47 +0530

    Implemented Merge Sort

---
+++ sorting/merge-sort.c
@@ -0,0 +1,59 @@
+// Merge Sort algorithm sorting algo with time complexity of O(nlogn)
+#include <stdio.h>
+
+void merge(int * arr, int s, int e){
+    if(s<e){
+        int arr1[e-s+1];
+        int m = (s+e)/2;
+        int i=s;
+        int j=m+1;
+        int k;
+        for(k=0; k<e-s+1; k++){
+            if(i==m+1 || j==e+1){
+                break;
+            }
+            if(arr[i]<arr[j]){
+                arr1[k] = arr[i];
+                i++;
+            }else{
+                arr1[k] = arr[j];
+                j++;
+            }
+        }
+        while(i<m+1){
+            arr1[k] = arr[i];
+            k++;
+            i++;
+        }
+
+        while(j<e+1){
+            arr1[k] = arr[j];
+            k++;
+            j++;
+        }
+
+        for(int k=0; k<e-s+1; k++){
+            arr[s+k] = arr1[k];
+        }
+    }
+}
+
+void mergeSort(int * arr, int s, int e){
+    if(s<e){
+        int m = (s+e)/2;
+        mergeSort(arr, s, m);
+        mergeSort(arr, m+1, e);
+        merge(arr, s, e);
+    }
+}
+
+int main(){
+    int arr[9] = {2,5,3,4,1,8,9,6,12};
+    mergeSort(arr, 0, 8);
+
+    for(int i=0; i<9; i++){
+        printf("%d\n", arr[i]);
+    }
+
+    return 0;
+}
